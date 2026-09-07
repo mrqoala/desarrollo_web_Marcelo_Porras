f965157d7797b84ea4224d0ce2ec81e17eac7d90
Marcelo Porras <marceloporras2@gmail.com>
2026-09-07 15:28:40 -0300

    Added: Agregar_Avistamiento

--- agregar_avistamiento.js
+++ agregar_avistamiento.js
@@ -1,36 +1,46 @@
 
-const validadorForm= (event) =>{
+class Avistamiento{
+    constructor(nombreAve,tipoAve,fecha,region,comuna,imagen){
+        this.nombreAve=nombreAve;
+        this.tipoAve=tipoAve;
+        this.region=region;
+        this.fecha=fecha;
+        this.comuna=comuna;
+        this.imagen=imagen;
+    }
+}
+
+const validadorAgregar= (event) =>{
     event.preventDefault();
     const validadorNombre = (nombre)=> nombre && nombre.length>3;
     const seEligioOpcion = (opcion) => opcion !=="";
-    const numeroVálido =(numero) =>{
-        const regexchile= /^(\+?56)?(\s?)(0?9)(\s?)[98765432]\d{7}$/;
-        return regexchile.test(numero);
-    }
-    let nombre= document.getElementById("nombre");
+    const subioFoto = (foto) => foto.files.length===0;
+
+    let nombreAve= document.getElementById("nombre-ave");
     let region =document.getElementById("region");
     let comuna = document.getElementById("comuna");
-    let numero = document.getElementById("telefono");
-    let isValid = validadorNombre(nombre.value) && seEligioOpcion(region.value) && seEligioOpcion(comuna.value)&& numeroVálido(numero.value);
+    let tipoAve = document.getElementById("tipo-ave");
+    let imagen=document.getElementById("fotoave");
+    let isValid = validadorNombre(nombreAve.value) && seEligioOpcion(region.value) && seEligioOpcion(comuna.value)&& seEligioOpcion(tipoAve.value)&& !(subioFoto(imagen));
+    let fecha=document.getElementById("fecha-avistamiento");
 
     let errorNombre=document.getElementById("error-nombre");
     errorNombre.className="error";
     
-    let errorTelefono =document.getElementById("error-telefono");
-    errorTelefono.className="error";
-
     let errorRegion =document.getElementById("error-region");
     errorRegion.className="error";
 
-    let errorComuna = document.getElementById("error-comuna")
+    let errorComuna = document.getElementById("error-comuna");
     errorComuna.className="error";
+    
+    let errorTipo =document.getElementById("error-tipo");
+    errorTipo.className="error";
+    let errorFoto=document.getElementById("error-foto");
+    errorFoto.className="error";
 
     if (!isValid){
-        if (!validadorNombre(nombre.value)){
+        if (!validadorNombre(nombreAve.value)){
             errorNombre.className="error.visible";
-        }
-        if (!numeroVálido(numero.value)){
-            errorTelefono.className="error.visible"
         }
         if (!seEligioOpcion(region.value)){
             errorRegion.className="error.visible"
@@ -38,14 +48,22 @@ const validadorForm= (event) =>{
         if (!seEligioOpcion(comuna.value)){
             errorComuna.className="error.visible"
         }
-    }
+        if(!seEligioOpcion(tipoAve.value)){
+            errorTipo.className="error.visible";
+        }
+        if (subioFoto(imagen)){
+            errorFoto.className="error.visible";
+        }
+
+        }
     else{
         window.location.href="index.html";
-        window.alert("Registro Exitoso")
+        window.alert("Agregar Exitoso")
     }
 }
 let boton=document.querySelector("button[type=submit]");
-boton.addEventListener("click",validadorForm);
+boton.addEventListener("click",validadorAgregar);
+
 
 async function cargarRegiones() {
   const regiones = await fetch('./regiones.json');
@@ -68,14 +86,11 @@ async function cargarRegiones() {
             comunaSelector.firstChild.remove();
         }
         const comunas = datos.regions[id].communes.map(r=>r.name);
-        for(comuna in comunas){
+        for(const comuna in comunas){
             let nuevaComuna=document.createElement("option");
             let text=document.createTextNode(comunas[comuna]);
             nuevaComuna.appendChild(text);
             comunaSelector.appendChild(nuevaComuna);
-    }
-});
-};
-cargarRegiones();
-
-
+    };
+    })};
+cargarRegiones()
