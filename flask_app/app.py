d67c906192a2fe0ea7b9139c41e1a512e0cd4dfb
Marcelo Porras <marceloporras2@gmail.com>
2026-09-18 14:45:20 -0300

    change: restructuración del las carpetas

---
+++ flask_app/app.py
@@ -0,0 +1,9 @@
+from flask import Flask, request, render_template, redirect, url_for, session
+from utils.validations import validate_login_user, validate_register_user, validate_confession
+from database import db
+from werkzeug.utils import secure_filename
+import hashlib
+import filetype
+import os
+
+UPLOAD_FOLDER = 'static/uploads'
